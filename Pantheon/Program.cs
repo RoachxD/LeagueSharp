60763c1bc822293f0d36ff4ae93812a60bfbb75c
Roach_ <roachxxxx@gmail.com>
2014-10-29 00:34:37 +0200

    Updated :ok_hand:
    - Fixed ATR Problems
    - Indented better the Script

--- Pantheon/Program.cs
+++ Pantheon/Program.cs
@@ -49,39 +49,39 @@ namespace Pantheon
             Config = new Menu(CharName, CharName, true);
 
             Config.AddSubMenu(new Menu("Combo Settings", "combo"));
-                Config.SubMenu("combo").AddItem(new MenuItem("comboKey", "Full Combo Key").SetValue(new KeyBind(32, KeyBindType.Press)));
-                Config.SubMenu("combo").AddItem(new MenuItem("comboItems", "Use Items with Burst").SetValue(true));
-                Config.SubMenu("combo").AddItem(new MenuItem("autoIgnite", "Use Ignite with Burst").SetValue(true));
+            Config.SubMenu("combo").AddItem(new MenuItem("comboKey", "Full Combo Key").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Config.SubMenu("combo").AddItem(new MenuItem("comboItems", "Use Items with Burst").SetValue(true));
+            Config.SubMenu("combo").AddItem(new MenuItem("autoIgnite", "Use Ignite with Burst").SetValue(true));
 
             Config.AddSubMenu(new Menu("Harass Settings", "harass"));
-                Config.SubMenu("harass").AddItem(new MenuItem("harassKey", "Harass Key").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
-                Config.SubMenu("harass").AddItem(new MenuItem("hMode", "Harass Mode: ").SetValue(new StringList(new[] { "Q", "W+E" })));
-                Config.SubMenu("harass").AddItem(new MenuItem("autoQ", "Auto-Q when Target in Range").SetValue(new KeyBind("Z".ToCharArray()[0], KeyBindType.Toggle)));
-                Config.SubMenu("harass").AddItem(new MenuItem("aQT", "Don't Auto-Q if in enemy Turret Range").SetValue(true));
-                Config.SubMenu("harass").AddItem(new MenuItem("harassMana", "Min. Mana Percent: ").SetValue(new Slider(50)));
+            Config.SubMenu("harass").AddItem(new MenuItem("harassKey", "Harass Key").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+            Config.SubMenu("harass").AddItem(new MenuItem("hMode", "Harass Mode: ").SetValue(new StringList(new[] { "Q", "W+E" })));
+            Config.SubMenu("harass").AddItem(new MenuItem("autoQ", "Auto-Q when Target in Range").SetValue(new KeyBind("Z".ToCharArray()[0], KeyBindType.Toggle)));
+            Config.SubMenu("harass").AddItem(new MenuItem("aQT", "Don't Auto-Q if in enemy Turret Range").SetValue(true));
+            Config.SubMenu("harass").AddItem(new MenuItem("harassMana", "Min. Mana Percent: ").SetValue(new Slider(50)));
 
             Config.AddSubMenu(new Menu("Farming Settings", "farm"));
-                Config.SubMenu("farm").AddItem(new MenuItem("farmKey", "Farming Key").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
-                Config.SubMenu("farm").AddItem(new MenuItem("qFarm", "Farm with Spear Shot (Q)").SetValue(true));
-                Config.SubMenu("farm").AddItem(new MenuItem("wFarm", "Farm with Aegis of Zeonia (W)").SetValue(true));
-                Config.SubMenu("farm").AddItem(new MenuItem("farmMana", "Min. Mana Percent: ").SetValue(new Slider(50)));
+            Config.SubMenu("farm").AddItem(new MenuItem("farmKey", "Farming Key").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+            Config.SubMenu("farm").AddItem(new MenuItem("qFarm", "Farm with Spear Shot (Q)").SetValue(true));
+            Config.SubMenu("farm").AddItem(new MenuItem("wFarm", "Farm with Aegis of Zeonia (W)").SetValue(true));
+            Config.SubMenu("farm").AddItem(new MenuItem("farmMana", "Min. Mana Percent: ").SetValue(new Slider(50)));
 
             Config.AddSubMenu(new Menu("Jungle Clear Settings", "jungle"));
-                Config.SubMenu("jungle").AddItem(new MenuItem("jungleKey", "Jungle Clear Key").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
-                Config.SubMenu("jungle").AddItem(new MenuItem("qJungle", "Farm with Spear Shot (Q)").SetValue(true));
-                Config.SubMenu("jungle").AddItem(new MenuItem("wJungle", "Farm with Aegis of Zeonia (W)").SetValue(true));
-                Config.SubMenu("jungle").AddItem(new MenuItem("eJungle", "Farm with Heartseeker Strike (E)").SetValue(true));
+            Config.SubMenu("jungle").AddItem(new MenuItem("jungleKey", "Jungle Clear Key").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+            Config.SubMenu("jungle").AddItem(new MenuItem("qJungle", "Farm with Spear Shot (Q)").SetValue(true));
+            Config.SubMenu("jungle").AddItem(new MenuItem("wJungle", "Farm with Aegis of Zeonia (W)").SetValue(true));
+            Config.SubMenu("jungle").AddItem(new MenuItem("eJungle", "Farm with Heartseeker Strike (E)").SetValue(true));
 
             Config.AddSubMenu(new Menu("Draw Settings", "drawing"));
-                Config.SubMenu("drawing").AddItem(new MenuItem("mDraw", "Disable All Range Draws").SetValue(false));
-                Config.SubMenu("drawing").AddItem(new MenuItem("Target", "Draw Circle on Target").SetValue(new Circle(true, System.Drawing.Color.FromArgb(255, 255, 0, 0))));
-                Config.SubMenu("drawing").AddItem(new MenuItem("QDraw", "Draw Spear Shot (Q) Range").SetValue(new Circle(true, System.Drawing.Color.FromArgb(255, 178, 0, 0))));
-                Config.SubMenu("drawing").AddItem(new MenuItem("WDraw", "Draw Aegis of Zeonia (W) Range").SetValue(new Circle(false, System.Drawing.Color.FromArgb(255, 32, 178, 170))));
-                Config.SubMenu("drawing").AddItem(new MenuItem("EDraw", "Draw Heartseeker Strike (E) Range").SetValue(new Circle(true, System.Drawing.Color.FromArgb(255, 128, 0, 128))));
+            Config.SubMenu("drawing").AddItem(new MenuItem("mDraw", "Disable All Range Draws").SetValue(false));
+            Config.SubMenu("drawing").AddItem(new MenuItem("Target", "Draw Circle on Target").SetValue(new Circle(true, System.Drawing.Color.FromArgb(255, 255, 0, 0))));
+            Config.SubMenu("drawing").AddItem(new MenuItem("QDraw", "Draw Spear Shot (Q) Range").SetValue(new Circle(true, System.Drawing.Color.FromArgb(255, 178, 0, 0))));
+            Config.SubMenu("drawing").AddItem(new MenuItem("WDraw", "Draw Aegis of Zeonia (W) Range").SetValue(new Circle(false, System.Drawing.Color.FromArgb(255, 32, 178, 170))));
+            Config.SubMenu("drawing").AddItem(new MenuItem("EDraw", "Draw Heartseeker Strike (E) Range").SetValue(new Circle(true, System.Drawing.Color.FromArgb(255, 128, 0, 128))));
 
             Config.AddSubMenu(new Menu("Misc Settings", "misc"));
-                Config.SubMenu("misc").AddItem(new MenuItem("stopChannel", "Interrupt Channeling Spells").SetValue(true));
-                Config.SubMenu("misc").AddItem(new MenuItem("usePackets", "Use Packets to Cast Spells").SetValue(false));
+            Config.SubMenu("misc").AddItem(new MenuItem("stopChannel", "Interrupt Channeling Spells").SetValue(true));
+            Config.SubMenu("misc").AddItem(new MenuItem("usePackets", "Use Packets to Cast Spells").SetValue(false));
 
             Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
             Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
@@ -113,7 +113,7 @@ namespace Pantheon
             var farmKey = Config.Item("farmKey").GetValue<KeyBind>().Active;
             var jungleClearKey = Config.Item("jungleKey").GetValue<KeyBind>().Active;
 
-            Orbwalker.SetAttacks(!UsingEorR());
+            Orbwalker.SetAttack(!UsingEorR());
             Orbwalker.SetMovement(!UsingEorR());
 
             if (comboKey && target != null)
@@ -130,7 +130,7 @@ namespace Pantheon
                     JungleClear();
 
                 if (Config.Item("autoQ").GetValue<KeyBind>().Active && target != null)
-                    if (Config.Item("aQT").GetValue<bool>() ? !Utility.UnderTurret(ObjectManager.Player, true) : Utility.UnderTurret(ObjectManager.Player, true) && ObjectManager.Player.Distance(target) <= Q.Range)
+                    if (Config.Item("aQT").GetValue<bool>() ? !Utility.UnderTurret(ObjectManager.Player, true) : Utility.UnderTurret(ObjectManager.Player, true) && ObjectManager.Player.Distance(target) <= Q.Range && Q.IsReady())
                         Q.CastOnUnit(target, Config.Item("usePackets").GetValue<bool>());
             }
         }
@@ -213,11 +213,13 @@ namespace Pantheon
             switch (menuItem)
             {
                 case 0:
-                    Q.CastOnUnit(target, Config.Item("usePackets").GetValue<bool>());
+                    if (Q.IsReady())
+                        Q.CastOnUnit(target, Config.Item("usePackets").GetValue<bool>());
                     break;
                 case 1:
-                    W.CastOnUnit(target, Config.Item("usePackets").GetValue<bool>());
-                    if (!target.CanMove)
+                    if (W.IsReady())
+                        W.CastOnUnit(target, Config.Item("usePackets").GetValue<bool>());
+                    if (!W.IsReady() && E.IsReady())
                         E.Cast(target, Config.Item("usePackets").GetValue<bool>());
                     break;
             }
@@ -234,8 +236,11 @@ namespace Pantheon
 
             if (Config.Item("qFarm").GetValue<bool>() && Q.IsReady())
             {
-                foreach (var minion in from minion in minions let actualHp = (HealthPrediction.GetHealthPrediction(minion, (int)(ObjectManager.Player.Distance(minion) * 1000 / 1500)) <= minion.MaxHealth * 0.15) ? ObjectManager.Player.GetSpellDamage(minion, SpellSlot.Q) * 2 : ObjectManager.Player.GetSpellDamage(minion, SpellSlot.Q) where minion.IsValidTarget() && HealthPrediction.GetHealthPrediction(minion,
-                    (int) (ObjectManager.Player.Distance(minion)* 1000 / 1500)) <= actualHp select minion)
+                foreach (var minion in from minion in minions
+                                       let actualHp = (HealthPrediction.GetHealthPrediction(minion, (int)(ObjectManager.Player.Distance(minion) * 1000 / 1500)) <= minion.MaxHealth * 0.15) ? ObjectManager.Player.GetSpellDamage(minion, SpellSlot.Q) * 2 : ObjectManager.Player.GetSpellDamage(minion, SpellSlot.Q)
+                                       where minion.IsValidTarget() && HealthPrediction.GetHealthPrediction(minion,
+                                           (int)(ObjectManager.Player.Distance(minion) * 1000 / 1500)) <= actualHp
+                                       select minion)
                 {
                     Q.CastOnUnit(minion, Config.Item("usePackets").GetValue<bool>());
                     return;
@@ -285,7 +290,7 @@ namespace Pantheon
             if (IgniteSlot != SpellSlot.Unknown && ObjectManager.Player.SummonerSpellbook.CanUseSpell(IgniteSlot) == SpellState.Ready)
                 dmg += ObjectManager.Player.GetSummonerSpellDamage(target, Damage.SummonerSpell.Ignite);
 
-            return (float) dmg;
+            return (float)dmg;
         }
 
         public static void UseItems(Obj_AI_Base target)
