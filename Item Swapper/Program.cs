e132987245365a2adf61f15f4de9420369803085
RoachxD <roachxxxx@gmail.com>
2014-12-27 18:51:51 +0200

    Improved a little

--- Item Swapper/Program.cs
+++ Item Swapper/Program.cs
@@ -12,7 +12,6 @@ namespace Item_Swapper
     internal class Program
     {
         private static byte _firstKey = 0x60;
-
         private static readonly byte[] Keys = {0x64, 0x65, 0x66, 0x61, 0x62, 0x63};
 
         private static void Main(string[] args)
@@ -31,18 +30,26 @@ namespace Item_Swapper
         private static void Game_OnWndProc(WndEventArgs args)
         {
             if (args.Msg == 0x0100 && args.WParam == 0x60)
+            {
                 _firstKey = 0x60;
+            }
 
             if (args.Msg != 0x0100 || !Keys.ToList().Contains((byte) args.WParam))
+            {
                 return;
+            }
 
             var key = (byte) args.WParam;
 
             if (_firstKey == 0x60)
+            {
                 _firstKey = key;
+            }
 
             if (_firstKey == key)
+            {
                 return;
+            }
 
             Packet.C2S.SwapItem.Encoded(new Packet.C2S.SwapItem.Struct((byte) Array.IndexOf(Keys, _firstKey),
                 (byte) Array.IndexOf(Keys, key), ObjectManager.Player.NetworkId)).Send();
@@ -51,7 +58,9 @@ namespace Item_Swapper
         private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
         {
             if (args.PacketData[0] == Packet.S2C.SwapItemAns.Header)
+            {
                 _firstKey = 0x60;
+            }
         }
     }
 }
